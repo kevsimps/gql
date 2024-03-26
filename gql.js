bb83ee9e30a86119a63343d86a41bc9fc42a44d4
Kevin Simpson (kevsimps) <kevsimps@cisco.com>
2024-03-25 23:04:04 -0400

    script to remove escape slashes

--- gql.js
+++ gql.js
@@ -2,3 +2,6 @@ function vidPop(id){
     video = new CustomEvent("vidPop",{bubbles: true, composed: true, detail: id })
     document.body.dispatchEvent(video)
     }
+function slash(){
+    Array.from(document.querySelectorAll("textarea")).forEach((element) => {element.value = element.value.replaceAll("\\",""); console.log(element.value)})
+}
